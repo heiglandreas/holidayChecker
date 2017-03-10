9fc29f4c857c35c6c9127760510d18835d8363e4
Andreas Heigl <andreas@heigl.org>
2017-03-10 18:20:33 +0100

    Adds british holidays
    For that I had to add a date-class that handles follows ups as the
    british shift holidfays to the following weekday when they are during a
    weekend. Awesome :)!

---
+++ src/IteratorItem/DateFollowUp.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Copyright (c) Andreas Heigl<andreas@heigl.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author    Andreas Heigl<andreas@heigl.org>
+ * @copyright Andreas Heigl
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT-License
+ * @since     08.03.2017
+ * @link      http://github.com/heiglandreas/org.heigl.Holidaychecker
+ */
+
+namespace Org_Heigl\Holidaychecker\IteratorItem;
+
+use Org_Heigl\Holidaychecker\HolidayIteratorItemInterface;
+
+class DateFollowUp implements HolidayIteratorItemInterface
+{
+    private $day;
+
+    private $month;
+
+    private $holiday;
+
+    private $name;
+
+    private $followup;
+
+    public function __construct(string $name, bool $holiday, int $day, int $month, string $followup)
+    {
+        $this->day = $day;
+        $this->month = $month;
+        $this->followup = $followup;
+        $this->holiday = $holiday;
+        $this->name = $name;
+    }
+
+    public function dateMatches(\DateTimeInterface $date) : bool
+    {
+        $day = new \DateTimeImmutable($date->format('Y') . '-' . $this->month . '-' . $this->day);
+        if ($day->format('w') === "0" || $day->format('w') === "6") {
+            $day = $day->modify('next ' . $this->followup);
+        }
+
+        return $date->format('Y-m-d') === $day->format('Y-m-d');
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function isHoliday(): bool
+    {
+        return $this->holiday;
+    }
+}
